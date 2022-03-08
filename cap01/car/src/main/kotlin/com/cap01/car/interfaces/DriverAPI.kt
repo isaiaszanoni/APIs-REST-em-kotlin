279fe9051cb9873d8eedefdb740054160a5ee743
isaiaszanoni <isaiasz.mus@gmail.com>
2022-03-07 23:02:25 -0300

    Bug(DriverAPI): adiciona uma exceção quando o driver_id nao existe

--- cap01/car/src/main/kotlin/com/cap01/car/interfaces/DriverAPI.kt
+++ cap01/car/src/main/kotlin/com/cap01/car/interfaces/DriverAPI.kt
@@ -2,12 +2,14 @@ package com.cap01.car.interfaces
 
 import com.cap01.car.domain.Driver
 import com.cap01.car.domain.DriverRepository
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @Service
 @RestController
@@ -20,5 +22,6 @@ class DriverAPI (
 
     @GetMapping("drivers/{id}")
     fun findDriver(@PathVariable("id") id: Long) =
-        driverRepository.findById(id).get()
+        driverRepository.findById(id)
+            .orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND) }
 }
