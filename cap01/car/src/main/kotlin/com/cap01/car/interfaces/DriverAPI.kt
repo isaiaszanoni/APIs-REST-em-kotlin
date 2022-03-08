f5dad126fadb33bc027e7e342bd9a88b421628e7
isaiaszanoni <isaiasz.mus@gmail.com>
2022-03-07 22:30:43 -0300

    feat(DriverAPI): adicao do metodo findDriver

--- cap01/car/src/main/kotlin/com/cap01/car/interfaces/DriverAPI.kt
+++ cap01/car/src/main/kotlin/com/cap01/car/interfaces/DriverAPI.kt
@@ -5,6 +5,7 @@ import com.cap01.car.domain.DriverRepository
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -16,4 +17,8 @@ class DriverAPI (
 ) {
     @GetMapping("/drivers")
     fun listDrivers() = driverRepository.findAll()
+
+    @GetMapping("drivers/{id}")
+    fun findDriver(@PathVariable("id") id: Long) =
+        driverRepository.findById(id).get()
 }
